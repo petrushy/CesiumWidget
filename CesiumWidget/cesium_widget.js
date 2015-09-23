76d66814f5c845ae75ca98499d5fd7ea76716f88
Massimo Di Stefano <epiesasha@me.com>
2015-09-23 17:53:36 -0400

    Merge branch 'cameraviews'
    # Conflicts:
    #	CesiumWidget/cesium_widget.js
    #	CesiumWidget/cesiumwidget.py

--- CesiumWidget/cesium_widget.js
+++ CesiumWidget/cesium_widget.js
@@ -122,7 +122,10 @@ define(
                 this.model.on('change:kml', this.update_kml, this);
                 this.update_geojson();
                 this.model.on('change:geojson', this.update_geojson, this);
-
+                this.fly_to();
+                this.model.on('change:flyto', this.fly_to, this);
+                this.zoom_to();
+                this.model.on('change:zoomto', this.zoomto, this);
                 // call __super__.update to handle housekeeping
                 //return CesiumView.__super__.update.apply(this, arguments);
             },
