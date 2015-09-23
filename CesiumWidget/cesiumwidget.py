97b6497086dcd4284f3535c1edc679cb762dff01
Massimo Di Stefano <epiesasha@me.com>
2015-09-23 16:54:13 -0400

    Added options to change camera settings (fly_to little buggy, it updates the position only when the position changes.doen't work if only roll-pich-heading are changing )

--- CesiumWidget/cesiumwidget.py
+++ CesiumWidget/cesiumwidget.py
@@ -11,6 +11,9 @@ class CesiumWidget(DOMWidget):
 
     czml = Unicode(sync=True)
     kml_url = Unicode(sync=True)
+
+    zoomto = Unicode(sync=True)
+    flyto = Unicode(sync=True)
     
     animation = Bool(True, sync=True)
     base_layer_picker = Bool(True, sync=True)
